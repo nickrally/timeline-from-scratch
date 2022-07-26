fdfacb6169b28fcb5536da96ebb1a80532c22d6a
nickrally <nickrally2009@gmail.com>
2022-07-25 22:03:10 -0600

    grid wip

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,13 +1,30 @@
 import React, { useRef } from "react";
 import { useContainerWidth } from "../utils/useContainerWidth";
+import { useViewModelContext } from "../../context/ViewModelContext";
 import "./Timeline.css";
+import moment from "moment";
 
 const Timeline = ({ bars }) => {
+  const { endDate, startDate } = useViewModelContext();
+  const start = moment(startDate, "YYYY-MM-DD");
+  const end = moment(endDate, "YYYY-MM-DD");
+  const numberOfWeeks = Math.ceil(moment.duration(end.diff(start)).asWeeks());
   const timelineRef = useRef();
   const width = useContainerWidth(timelineRef);
+  const weekColumnWidth = Math.ceil(width / numberOfWeeks);
+  //console.log("numberOfDays:", numberOfDays, "width:", width);
+  //console.log("dayColumnWidth", dayColumnWidth);
+
   return (
     <>
       <div ref={timelineRef} className="container">
+        {new Array(numberOfWeeks).fill(0).map((_, idx) => (
+          <div
+            key={idx}
+            className="vl"
+            style={{ right: `${idx * weekColumnWidth}px` }}
+          ></div>
+        ))}
         {bars?.map((bar, idx) => (
           <span className="row" key={idx}>
             {bar}
